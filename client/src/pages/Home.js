574e03723e19412d51d37a59d33c22ce23c4ae05
Mufolk <guilhermemelo138@gmail.com>
2021-08-14 11:19:53 -0300

    refactoring fetch

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,11 +3,14 @@ import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
 function Home() {
-  const { loading, data } = useQuery(FETCH_USERS_QUERY);
-  if (data === undefined) {
+  const {
+    loading,
+    data: { getUsers: users },
+  } = useQuery(FETCH_USERS_QUERY);
+  if (users === undefined) {
     return <h1>Loading</h1>;
   } else {
-    console.log(data);
+    console.log(users);
     return (
       <div>
         <h1>Home Page</h1>
