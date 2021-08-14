01a25cf202bb46abe15b98d1565d80093a4f6ab3
Mufolk <guilhermemelo138@gmail.com>
2021-08-14 11:18:05 -0300

    fetching data from graphql api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
 function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+  const { loading, data } = useQuery(FETCH_USERS_QUERY);
+  if (data === undefined) {
+    return <h1>Loading</h1>;
+  } else {
+    console.log(data);
+    return (
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    );
+  }
 }
+
+const FETCH_USERS_QUERY = gql`
+  {
+    getUsers {
+      id
+      username
+      email
+      createdAt
+    }
+  }
+`;
 
 export default Home;
