7a911ffeeebc33f13a64d4859b641a59d2527f54
Mufolk <guilhermemelo138@gmail.com>
2021-08-15 15:11:53 -0300

    implementing pwa

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import ApolloProvider from "./ApolloProvider";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
@@ -8,3 +9,5 @@ ReactDOM.render(ApolloProvider, document.getElementById("root"));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
