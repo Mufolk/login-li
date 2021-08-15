bcc432b6bb477248ecbeedf7319bc7196f582840
Mufolk <guilhermemelo138@gmail.com>
2021-08-15 12:28:18 -0300

    Fixing the PORT bug

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
